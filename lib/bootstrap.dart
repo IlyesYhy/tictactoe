ef6ae8b175200719ce6a2dc4b440c8b663547094
Ilyes Yhy <ilyes.yyi@gmail.com>
2026-05-27 16:23:57 +0200

    refactor(app): centralize composition root overrides

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -2,18 +2,9 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:package_info_plus/package_info_plus.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 import 'app/app.dart';
-import 'core/di/shared_preferences_provider.dart';
-import 'features/settings/data/repositories/local_settings_repository.dart';
-import 'features/settings/di/settings_providers.dart';
-import 'features/settings/domain/entities/app_language.dart';
-import 'features/settings/domain/entities/app_settings.dart';
-import 'features/settings/domain/entities/app_theme_mode.dart';
-import 'features/stats/data/repositories/local_stats_repository.dart';
-import 'features/stats/di/stats_providers.dart';
+import 'app/di/app_overrides.dart';
 
 Future<void> bootstrap() {
   return runZonedGuarded(
@@ -24,37 +15,9 @@ Future<void> bootstrap() {
             FlutterError.presentError(details);
           };
 
-          final sharedPreferences = await SharedPreferences.getInstance();
+          final overrides = await buildAppOverrides();
 
-          final settingsRepository = LocalSettingsRepository(sharedPreferences);
-          final initialSettings = AppSettings(
-            language:
-                (await settingsRepository.getLanguage()) ?? AppLanguage.en,
-            themeMode:
-                (await settingsRepository.getThemeMode()) ??
-                AppThemeMode.system,
-            isHapticFeedbackEnabled:
-                (await settingsRepository.getHapticFeedback()) ?? true,
-          );
-
-          final statsRepository = LocalStatsRepository(sharedPreferences);
-          final initialMatchHistory = await statsRepository.getMatchHistory();
-
-          final packageInfo = await PackageInfo.fromPlatform();
-
-          runApp(
-            ProviderScope(
-              overrides: [
-                sharedPreferencesProvider.overrideWithValue(sharedPreferences),
-                initialSettingsProvider.overrideWithValue(initialSettings),
-                initialMatchHistoryProvider.overrideWithValue(
-                  initialMatchHistory,
-                ),
-                packageInfoProvider.overrideWithValue(packageInfo),
-              ],
-              child: const App(),
-            ),
-          );
+          runApp(ProviderScope(overrides: overrides, child: const App()));
         },
         (error, stackTrace) {
           // In production, errors could be reported to an error tracking service
